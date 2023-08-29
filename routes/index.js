468b668d1dc1d3e2436beec38713dba7d29cf63d
Bogdddan <bogdankrajnik95@gmail.com>
2023-08-29 19:21:07 +0300

    налаштування роуту user

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 const contqactRoutes = require("./contacts");
+const authRoutes = require('./auth');
+const userRoutes = require('./users');
 
+const auth = require('../middleware/auth');
+
+router.use('/auth', authRoutes);
 router.use("/contacts", contqactRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
